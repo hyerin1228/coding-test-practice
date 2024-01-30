9692a5fd6661bb196eea31a696cb76b0a75db248
hyerin <84621142+hyerin1228@users.noreply.github.com>
2024-01-30 12:32:24 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -0,0 +1,24 @@
+function solution(seoul) {
+    var answer = '';
+    
+    for(let i=0; i<seoul.length; i++){
+        if(seoul[i] === "Kim")
+            answer = `김서방은 ${i}에 있다`;
+    }
+    
+    return answer;
+}
+
+
+
+
+
+/* 24.1.30(화)
+
+- 변수명 :
+- 문제 풀이 과정 : 
+            1) 배열 순회하며 인덱스 찾아내면 될 것 같은데.. 음...
+- 알게된 사실 : 
+        1) 
+
+*/
